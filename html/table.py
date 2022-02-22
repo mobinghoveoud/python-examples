6574fad5bb0e192dc9a8daaa0c98f0093aa041a7
Mohammad Mobin Ghoveoud <mobin.sgh@gmail.com>
2022-02-22 21:14:46 +0330

    Add html practice
    Create table from sample.csv
    with html

---
+++ html/table.py
@@ -0,0 +1,39 @@
+import re
+import webbrowser
+
+
+# bold & italic
+def format(text):
+    match = re.search("\*(.*)\*", text)
+    if match:
+        return f"<b>{match.group(1)}</b>"
+
+    match = re.search("_(.*)_", text)
+    if re.search("_.*_", text):
+        return f"<i>{match.group(1)}</i>"
+
+    return text
+
+
+with open('sample.html', 'r') as f:
+    sampleHtml = f.read()
+
+text = ""
+with open('sample.csv', 'r') as f:
+    line = f.readline().rstrip().split(',')
+
+    text += "<tr>\n"
+    for title in line:
+        text += f"    <th>{format(title)}</th>\n"
+    text += "</tr>\n"
+
+    for line in f:
+        text += f"<tr>\n"
+        for t in line.rstrip().split(','):
+            text += f"    <td>{format(t)}</td>\n"
+        text += "</tr>\n"
+
+with open('final.html', 'w') as f:
+    f.write(sampleHtml.replace('@@tr@@', text))
+
+webbrowser.open_new_tab('final.html')
